580e6d64e341f8542d665940681ee10a743648f4
MauricioAMartinez <maoskate1@gmail.com>
2021-04-29 13:43:59 -0500

    Modificacion tablas, y comandos ELOQUENT

---
+++ Proyecto1/database/seeders/Orders_has_productsSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class Orders_has_productsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
